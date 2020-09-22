23a2975abbc2d771fb77106a9b727b6670e81917
noemi <noesanz_7@msn.com>
2020-09-22 11:45:08 +0200

    anadimos la lista de repositorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListasDeRepositorioComponent } from './listas-de-repositorio/listas-de-repositorio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListasDeRepositorioComponent
   ],
   imports: [
     BrowserModule,
