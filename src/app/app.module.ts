5ee69c43cec8647a9d2dca7cee0a4032c3bbec2b
abelfubu <abelfubu@gmail.com>
2020-09-22 15:28:40 +0200

    small fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { ProfileComponent } from './profile/profile.component';
-import { HeaderComponent } from './header/header.component';
-
-import { ListasDeRepositorioComponent } from './listas-de-repositorio/listas-de-repositorio.component';
 import { AsideComponent } from './aside/aside.component';
+import { HeaderComponent } from './header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile.component';
 import { UsuarioComponent } from './usuario/usuario.component';
-
+import { ListasDeRepositorioComponent } from './listas-de-repositorio/listas-de-repositorio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-
-    ProfileComponent,
-    HeaderComponent,
-
-    ListasDeRepositorioComponent,
-
     AsideComponent,
-
-    UsuarioComponent
-
+    HeaderComponent,
+    ProfileComponent,
+    UsuarioComponent,
+    ListasDeRepositorioComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [AsideComponent],
+  schemas: [],
 })
-export class AppModule { }
+export class AppModule {}
